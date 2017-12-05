3065522f726b462b910132393eb55ff90bb9d20c
Romain BERNARD <30secondstodraw@gmail.com>
2017-12-05 21:09:03 +0100

    Added R algorithm

---
+++ R_Dromigny.R
@@ -0,0 +1,16 @@
+#Trinome composé de : Romain BERNARD, Loïc BOUTIN, Ivan DROMIGNY
+#Auteur : Romain BERNARD
+
+r_atosthene = function(n) {
+  
+  tab = c(2:n)
+  i=1
+  
+  while (tab[i]^2 <= n) {
+    tab = tab[tab %% tab[i] != 0 | tab==tab[i]] #
+    i = i + 1
+  }
+  return (tab)
+}
+
+system.time(r_atosthene(1000000))
