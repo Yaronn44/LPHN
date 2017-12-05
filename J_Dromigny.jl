3362042e91874688b5d2194b3d854a50de91b1ce
Romain BERNARD <30secondstodraw@gmail.com>
2017-12-05 17:49:18 +0100

    Added time measurement and author name

--- J_Dromigny.jl
+++ J_Dromigny.jl
@@ -1,9 +1,12 @@
 #Ce projet a été réalisé en collaboration avec Romain Bernard et Loïc Boutin
+#Auteur : Loïc BOUTIN
 
 function crible(n)
+    tic()
     primes = [nb for nb in range(2,n)]                          #On génère tous les nombres de 2 à n
     for i in primes                                             #Pour chaque élément du tableau généré
         filter!((x -> x % i != 0 || x == i), primes)            #On supprime tous éléments divisible par ce premier (excepté lui-même)
     end
+    toc()
     return primes                                               #On retourne alors les éléments n'ayant pas été filtrés
 end
