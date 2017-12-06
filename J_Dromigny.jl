cdc619e90aa98ae573605eca0549a970e8b54349
Ivan <ivan-44@hotmail.fr>
2017-12-06 09:18:09 +0100

    put the timer outside the function

--- J_Dromigny.jl
+++ J_Dromigny.jl
@@ -2,15 +2,15 @@
 #Auteur : Loïc BOUTIN
 
 function crible(n)
-    tic()
     primes = [nb for nb in range(2,n)]                          #On génère tous les nombres de 2 à n
     for i in primes                                             #Pour chaque élément du tableau généré
         filter!((x -> x % i != 0 || x == i), primes)            #On supprime tous éléments divisible par ce premier (excepté lui-même)
     end
-    toc()
     return primes                                               #On retourne alors les éléments n'ayant pas été filtrés
 end
 
 print("Entrez la valeur pour le crible : ")
 val = parse(UInt32, readline())
+tic()
 crible(val)
+toc()
