75e6e8d2ad3d41a29b22354936202fb7e2ae1114
Romain BERNARD <30secondstodraw@gmail.com>
2017-12-06 14:11:59 +0100

    Added comments

--- R_Dromigny.R
+++ R_Dromigny.R
@@ -3,11 +3,14 @@
 
 r_atosthene = function(n) {
   
-  tab = c(2:n)
-  i=1
+  tab = c(2:n) #On omet immédiatement 1
+  i=1 #Itérateur dans le tableau
   
+  #Une fois que le carré de la plus grande valeur étudiée est supérieur à n, on n'a plus de nombres à supprimer => STOP
   while (tab[i]^2 <= n) {
-    tab = tab[tab %% tab[i] != 0 | tab==tab[i]] 
+    tab = tab[tab %% tab[i] != 0 | tab==tab[i]] #Syntaxe pour filtrer le tableau ressemblant à 
+                                                #la déclaration conditionelle d'un ensemble, ne pas oublier la seconde 
+                                                #partie de la condition, sinon des nombres premiers se suppriment eux-mêmes
     i = i + 1
   }
   return (tab)
